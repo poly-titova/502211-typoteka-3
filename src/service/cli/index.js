b2a1d06911de564ea5707b8f303271ab0852fa3b
Poly Titova <poly_titova@mail.ru>
2022-02-04 15:54:19 +0300

    Добавили обработку команды генерация

--- src/service/cli/index.js
+++ src/service/cli/index.js
@@ -1,9 +1,11 @@
 'use strict';
 
 const help = require(`./help`);
+const generate = require(`./generate`);
 const version = require(`./version`);
 
 const Cli = {
+  [generate.name]: generate,
   [help.name]: help,
   [version.name]: version,
 };
