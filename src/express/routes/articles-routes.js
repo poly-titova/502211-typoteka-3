99ab6325ff31e080a9d574c9cf8d94ddc467f67f
Poly Titova <poly_titova@mail.ru>
2022-03-05 19:27:15 +0300

    Добавили остальные шаблоны

--- src/express/routes/articles-routes.js
+++ src/express/routes/articles-routes.js
@@ -1,11 +1,11 @@
 'use strict';
 
-const { Router } = require(`express`);
+const {Router} = require(`express`);
 const articlesRouter = new Router();
 
-articlesRouter.get(`/category/:id`, (req, res) => res.send(`/articles/category/:id`));
-articlesRouter.get(`/add`, (req, res) => res.send(`/articles/add`));
-articlesRouter.get(`/edit/:id`, (req, res) => res.send(`/articles/edit/:id`));
-articlesRouter.get(`/:id`, (req, res) => res.send(`/articles/:id`));
+articlesRouter.get(`/category/:id`, (req, res) => res.render(`articles-by-category`));
+articlesRouter.get(`/add`, (req, res) => res.render(`post`));
+articlesRouter.get(`/edit/:id`, (req, res) => res.render(`post`));
+articlesRouter.get(`/:id`, (req, res) => res.render(`post-detail`));
 
 module.exports = articlesRouter;
