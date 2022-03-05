ef80af6ad1357ec057634012e455f5ae5d21f22c
Poly Titova <poly_titova@mail.ru>
2022-03-05 19:01:45 +0300

    Добавили шаблоны главного роутинга

--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -1,11 +1,11 @@
 'use strict';
 
-const { Router } = require(`express`);
+const {Router} = require(`express`);
 const mainRouter = new Router();
 
-mainRouter.get(`/`, (req, res) => res.send(`/`));
-mainRouter.get(`/register`, (req, res) => res.send(`/register`));
-mainRouter.get(`/login`, (req, res) => res.send(`/login`));
-mainRouter.get(`/search`, (req, res) => res.send(`/search`));
+mainRouter.get(`/`, (req, res) => res.render(`main`));
+mainRouter.get(`/register`, (req, res) => res.render(`sign-up`));
+mainRouter.get(`/login`, (req, res) => res.render(`login`));
+mainRouter.get(`/search`, (req, res) => res.render(`search`));
 
 module.exports = mainRouter;
