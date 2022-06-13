20368b0a2f130d1a8129b4f667787820bd3b6433
Poly Titova <poly_titova@mail.ru>
2022-06-13 17:43:08 +0300

    Сделали обработку маршрутов главной страницы

--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -2,8 +2,13 @@
 
 const {Router} = require(`express`);
 const mainRouter = new Router();
+const api = require(`../api`).getAPI();
 
-mainRouter.get(`/`, (req, res) => res.render(`main`));
+mainRouter.get(`/`, async (req, res) => {
+  const articles = await api.getArticles();
+  res.render(`main`, {articles});
+});
+
 mainRouter.get(`/register`, (req, res) => res.render(`sign-up`));
 mainRouter.get(`/login`, (req, res) => res.render(`login`));
 mainRouter.get(`/search`, (req, res) => res.render(`search`));
