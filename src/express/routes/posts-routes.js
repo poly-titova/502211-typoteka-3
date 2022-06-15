050f5e732cd4397457186a21a7c21dc5ae6a782c
Poly Titova <poly_titova@mail.ru>
2022-06-15 13:15:21 +0300

    Добавили редактирование постов

--- src/express/routes/posts-routes.js
+++ src/express/routes/posts-routes.js
@@ -2,10 +2,20 @@
 
 const {Router} = require(`express`);
 const postsRouter = new Router();
+const api = require(`../api`).getAPI();
 
 postsRouter.get(`/category/:id`, (req, res) => res.render(`articles-by-category`));
 postsRouter.get(`/add`, (req, res) => res.render(`post`));
-postsRouter.get(`/edit/:id`, (req, res) => res.render(`post`));
+
+postsRouter.get(`/edit/:id`, async (req, res) => {
+  const {id} = req.params;
+  const [article, categories] = await Promise.all([
+    api.getArticle(id),
+    api.getCategories()
+  ]);
+  res.render(`post-detail`, {article, categories});
+});
+
 postsRouter.get(`/:id`, (req, res) => res.render(`post-detail`));
 
 module.exports = postsRouter;
