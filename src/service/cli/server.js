d1adfbe3994ab6dba76d8c90aa5f8036a48c9606
Poly Titova <poly_titova@mail.ru>
2022-07-17 11:21:07 +0300

    Исправили ошибку при добавлении публикации

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -10,42 +10,42 @@ const logger = getLogger({name: `api`});
 const DEFAULT_PORT = 3000;
 const FILENAME = `mocks.json`;
 
-const app = express();
-app.use(express.json());
-
-app.get(`/articles`, async (req, res) => {
-  try {
-    const fileContent = await fs.readFile(FILENAME);
-    const mocks = JSON.parse(fileContent);
-    res.json(mocks);
-  } catch (_err) {
-    res.send([]);
-  }
-});
-
-app.use(API_PREFIX, appApi);
-
-app.use((req, res, next) => {
-  logger.debug(`Request on route ${req.url}`);
-  res.on(`finish`, () => {
-    logger.info(`Response status code ${res.statusCode}`);
-  });
-  next();
-});
-
-app.use((req, res) => {
-  res.status(HttpCode.NOT_FOUND)
-    .send(`Not found`);
-  logger.error(`Route not found: ${req.url}`);
-});
-
-app.use((err, _req, _res, _next) => {
-  logger.error(`An error occurred on processing request: ${err.message}`);
-});
-
 module.exports = {
   name: `--server`,
   run(args) {
+    const app = express();
+    app.use(express.json());
+
+    app.get(`/articles`, async (req, res) => {
+      try {
+        const fileContent = await fs.readFile(FILENAME);
+        const mocks = JSON.parse(fileContent);
+        res.json(mocks);
+      } catch (_err) {
+        res.send([]);
+      }
+    });
+
+    app.use(API_PREFIX, appApi);
+
+    app.use((req, res, next) => {
+      logger.debug(`Request on route ${req.url}`);
+      res.on(`finish`, () => {
+        logger.info(`Response status code ${res.statusCode}`);
+      });
+      next();
+    });
+
+    app.use((req, res) => {
+      res.status(HttpCode.NOT_FOUND)
+        .send(`Not found`);
+      logger.error(`Route not found: ${req.url}`);
+    });
+
+    app.use((err, _req, _res, _next) => {
+      logger.error(`An error occurred on processing request: ${err.message}`);
+    });
+
     const [customPort] = args;
     const port = Number.parseInt(customPort, 10) || DEFAULT_PORT;
 
