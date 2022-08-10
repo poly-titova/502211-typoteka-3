5368ec8278bc02009a1db94a4359338ea27df1e6
Poly Titova <poly_titova@mail.ru>
2022-08-10 13:20:45 +0300

    Описали связь между моделями

---
+++ src/service/models/index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const {Model} = require(`sequelize`);
+const defineCategory = require(`./category`);
+const defineComment = require(`./comment`);
+const defineArticle = require(`./article`);
+const Aliase = require(`./aliase`);
+
+class ArticleCategory extends Model {}
+
+const define = (sequelize) => {
+  const Category = defineCategory(sequelize);
+  const Comment = defineComment(sequelize);
+  const Article = defineArticle(sequelize);
+
+  Article.hasMany(Comment, {as: Aliase.COMMENTS, foreignKey: `articleId`, onDelete: `cascade`});
+  Comment.belongsTo(Article, {foreignKey: `articleId`});
+
+  Article.belongsToMany(Category, {through: `articleCategories`, as: Aliase.CATEGORIES});
+  Category.belongsToMany(Article, {through: `articleCategories`, as: Aliase.ARTICLES});
+
+  ArticleCategory.init({}, {sequelize});
+
+  Article.belongsToMany(Category, {through: ArticleCategory, as: Aliase.CATEGORIES});
+  Category.belongsToMany(Article, {through: ArticleCategory, as: Aliase.ARTICLES});
+  Category.hasMany(ArticleCategory, {as: Aliase.ARTICLE_CATEGORIES});
+
+  return {Category, Comment, Article, ArticleCategory};
+};
+
+module.exports = define;
