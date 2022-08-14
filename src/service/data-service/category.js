0773bf7b1030f1b0304abdac223675e3a2e20c51
Poly Titova <poly_titova@mail.ru>
2022-08-14 13:53:24 +0300

    Переписали сервисы данных

--- src/service/data-service/category.js
+++ src/service/data-service/category.js
@@ -1,17 +1,12 @@
 'use strict';
 
 class CategoryService {
-  constructor(articles) {
-    this._articles = articles;
+  constructor(sequelize) {
+    this._Category = sequelize.models.Category;
   }
 
-  findAll() {
-    const categories = this._articles.reduce((acc, article) => {
-      article.category.forEach((category) => acc.add(category));
-      return acc;
-    }, new Set());
-
-    return [...categories];
+  async findAll() {
+    return await this._Category.findAll({raw: true});
   }
 }
 
--- src/service/data-service/search.js
+++ src/service/data-service/search.js
@@ -1,15 +1,27 @@
 'use strict';
 
+const {Op} = require(`sequelize`);
+const Aliase = require(`../models/aliase`);
+
 class SearchService {
-  constructor(articles) {
-    this._articles = articles;
+  constructor(sequelize) {
+    this._Article = sequelize.models.Article;
   }
 
-  findAll(searchText) {
-    return this._articles.
-      filter((article) => article.title.includes(searchText));
+  async findAll(searchText) {
+    const articles = await this._Article.findAll({
+      where: {
+        title: {
+          [Op.substring]: searchText
+        }
+      },
+      include: [Aliase.CATEGORIES],
+      order: [
+        [`createdAt`, `DESC`]
+      ]
+    });
+    return articles.map((article) => article.get());
   }
-
 }
 
 module.exports = SearchService;
