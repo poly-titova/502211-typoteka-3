1c98c973607b41bb30e8cff1f0849ac03a4b9430
Poly Titova <poly_titova@mail.ru>
2022-12-29 22:33:47 +0300

    Сделали правки по оформлению

--- src/service/lib/sequelize.js
+++ src/service/lib/sequelize.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 const Sequelize = require(`sequelize`);
 
@@ -12,15 +12,15 @@ if (somethingIsNotDefined) {
 
 module.exports = new Sequelize(
   DB_NAME, DB_USER, DB_PASSWORD, {
-  host: DB_HOST,
-  port: DB_PORT,
-  dialect: `postgres`,
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 10000,
-    idle: 10000
-  },
-  logging: false
-}
+    host: DB_HOST,
+    port: DB_PORT,
+    dialect: `postgres`,
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 10000,
+      idle: 10000
+    },
+    logging: false
+  }
 );
